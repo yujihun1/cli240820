2bc1e5638993997caf6d2591d0edcd4899ca484e
yujihun1 <sk0253642@gmail.com>
2024-08-20 10:25:41 +0900

    5단계 출력 수정

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -49,7 +49,7 @@ public class App  {
             }else if (command.equals("목록")){
                 System.out.println("번호/ 제목 / 내용");
                 System.out.println("------------------");
-                for(int i=0; i<articleList.size();i++){
+                for(int i= articleList.size()-1; i>=0;i--){
                     Article article = articleList.get(i);
                     System.out.printf("%d,%s,%s\n",article.getId(),article.getSubject(),article.getContent());
                 }
